419d158329f61cb47a1b7efd68acab8b971f06b2
Earth <pimanus1234@gmail.com>
2022-06-06 01:07:50 +0700

    validator setup

--- LearnTypescript/nest-demo/src/pets/dto/create-pet.input.ts
+++ LearnTypescript/nest-demo/src/pets/dto/create-pet.input.ts
@@ -1,8 +1,11 @@
 import { Field, InputType } from "@nestjs/graphql";
+import { IsAlpha } from "class-validator";
 
 @InputType()
 
 export class CreatePetInput{
+
+    @IsAlpha()
     @Field()
     name: string;
 

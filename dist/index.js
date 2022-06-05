7ad1e3bba67b151aad6620d85a0d1065ba1d276d
Earth <pimanus1234@gmail.com>
2022-06-05 19:25:33 +0700

    learn type

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
-;
-let mySize = 2;
-console.log(mySize);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -41,11 +41,13 @@ calculateTax(10_000);
 */
 
 ///Object
-let employee: {
+/*
+type Employee = {
     readonly id: number,
     name: string,
     retire: (date: Date) => void
-} = {
+}
+let employee: Employee = {
     id: 1,
     name: '',
     retire: (date: Date) => {
@@ -53,3 +55,80 @@ let employee: {
     }
 };
 employee.name = 'Mosh';
+*/
+
+//Union type
+/*
+function kgToLbs(weight: number | string): number {
+    //Narrowing
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+
+kgToLbs(10);
+kgToLbs('10kg');
+*/
+
+// Intersection type
+/*
+type Draggable = {
+    drag: () => void
+};
+type Resizeable = {
+    resize: () => void
+};
+
+type UIWidget = Draggable & Resizeable; 
+
+let textbox: UIWidget = {
+    drag: () => { },
+    resize: () => {}
+}
+*/
+
+//Literial (exact)
+/*
+type Quantity = 50 | 100
+type Metric = 'cm' | 'inch';
+let quantity: Quantity = 100;
+*/
+
+//Nullable type
+/*
+function greet(name: string | null | undefined) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('Hola!');  
+}
+
+
+greet(null); 
+greet('hello');
+greet(undefined);
+*/
+
+//Optional chaining
+
+type Customer = {
+    birthday?: Date
+}
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(1);
+//optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+//Optional element access operator
+//
+//customers?.[0]
+
+//optional call
+let log: any = null;
+log?.('a');

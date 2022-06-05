fa94f1ab75ec7999daa87239f6230078d0f70620
Earth <pimanus1234@gmail.com>
2022-06-05 23:59:26 +0700

    DB intregation and Mutations

--- LearnTypescript/nest-demo/src/pets/pets.service.ts
+++ LearnTypescript/nest-demo/src/pets/pets.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePetInput } from './dto/create-pet.input';
 import { Pet } from './pet.entity';
 
 @Injectable()
 export class PetsService {
-    async findAll(): Promise<Pet[]>{
-        const pet = new Pet();
-        pet.id = 1;
-        pet.name = "Mambo";
+    constructor(@InjectRepository(Pet) private petsRepository: Repository<Pet>) {}
+    
+    createPet(createPetInput: CreatePetInput): Promise<Pet>{
+        const newPet = this.petsRepository.create(createPetInput); //newPet = new Pet(); new.name = input.name
+        
+        return this.petsRepository.save(newPet); //insert
+    }
 
-        return [pet];
+    async findAll(): Promise<Pet[]>{
+        return this.petsRepository.find(); //SELECT * pet
     }
 }
